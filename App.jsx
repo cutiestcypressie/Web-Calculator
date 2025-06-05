a1e47a05e1cc1a6002bd0715d640b88e79e34115
cutiestcypressie <bullo.cypress23@gmail.com>
2025-06-05 12:23:14 +0800

    adding components

--- App.jsx
+++ App.jsx
@@ -2,17 +2,7 @@ import React from "react";
 import Calculator from "./components/Calculator";
 
 function App() {
-  return (
-    <div style={{
-      backgroundColor: "#f5f5f5",
-      minHeight: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center"
-    }}>
-      <Calculator />
-    </div>
-  );
+  return <Calculator />;
 }
 
 export default App;
