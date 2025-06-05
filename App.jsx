11b5b1ef8062cdf338a2142e42e74e05f327e26d
cutiestcypressie <bullo.cypress23@gmail.com>
2025-06-05 12:00:24 +0800

    commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Calculator from "./components/Calculator";
+
+const App = () => {
+  return (
+    <div style={{ backgroundColor: "#f5f5f5", minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
